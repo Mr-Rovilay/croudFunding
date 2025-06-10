e28a53d7c35697acd7ef67a64c306cb91196baad
Mr-Rovilay <ayodejiakinola007@gmail.com>
2025-06-10 09:54:08 +0100

    entir

--- contracts/CroudFunding.sol
+++ contracts/CroudFunding.sol
@@ -33,12 +33,32 @@ _;
             deadline = block.timestamp + (_durationInDays * 1 days);
             owner = msg.sender;
         }
-        function fund() public payable {
-            require(msg.value > 0, "Must be greater than 0.");
+
+        function fund(uint256 _tierIndex) public payable {
             require(block.timestamp < deadline, "Campaign has ended");
+            require(_tierIndex < tiers.length, "tiers does not exits"); 
+            require(msg.value == tiers[_tierIndex].amount, "incorrect amount");
+            tiers[_tierIndex].backers++;
         }
-        function withdraw() public {
-            require(msg.sender == owner, "Not the creator of this contract");
+
+        function addTier( string memory _name,
+            uint256 _amount) public onlyOwner {
+                require(_amount > 0, "Amount must be greater than zero");
+
+                tiers.push(Tier(_name, _amount, 0));
+           
+
+        }
+
+        function removeTier(uint256 _index) public onlyOwner{
+            require(_index < tiers.length, "tires does not exits");
+            tiers[_index] = tiers[tiers.length -1];
+            tiers.pop();
+        }
+
+
+        function withdraw() public onlyOwner{
+
             require(address(this).balance >= goal, "Goal has not been reached");
 
             uint256 balance = address(this).balance;
